59d1d19a65b5167ef6833a10b5611ae1ef6cdfbb
Maribel Plaza <140650510+Maribel-17@users.noreply.github.com>
2024-06-28 12:40:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express ();
 
 app.get("/", (req, res) => {
-  res.send("Cac Deploy!!");
+  res.send("Cac Deploy");
 })
 
 const PORT = process.env.PORT || 3000;
